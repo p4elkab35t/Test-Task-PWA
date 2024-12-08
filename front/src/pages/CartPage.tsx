06af9bd3134499fbbeccbf2e515f397b597d6563
p4elkab35t <saganiter234@gmail.com>
2024-12-08 06:42:23 -0800

    add db and env in strapi

--- front/src/pages/CartPage.tsx
+++ front/src/pages/CartPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { RootState } from '../store';
@@ -12,11 +12,6 @@ type CartItemType = {
   Title: string;
   Price: number;
 };
-
-type CartRelationalType = {
-  id: number;
-};
-
 
 const CartPage: React.FC = () => {
   const dispatch = useDispatch();
